bb5cfbf67a82cf048183437ac01e2fe768565af6
lorisfo <loris-fo@outlook.com>
2015-02-15 16:27:42 +0000

    added a list of dishes

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,7 @@
 class Order
 
+  def dishes_choice
+    $dishes = {"carbonara" => 11, "bolognesa" => 9, "pesto" => 10, "sea food" => 13}
+  end
+	
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,4 +2,10 @@ require 'order'
 
 describe Order do
 
+let(:order)  {Order.new}
+
+	it 'should have a list of dishes' do
+		expect(order.dishes_choice.length).to eq (4)
+	end
+
 end
